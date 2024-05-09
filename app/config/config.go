df501b554abad9be379360019bce0f8c679f8395
alexpgates <alexpgates@gmail.com>
2024-05-09 09:14:43 -0500

    remove more unused config settings

--- app/config/config.go
+++ app/config/config.go
@@ -80,14 +80,7 @@ type Numbers struct {
 	DailyTopHour        int
 	DailyDiaryGoals     int
 	DailyDiaryGrateful  int
-	DailyDiaryDefault   int
 	DailyDiaryBest      int
-	DailyDiaryPeople    int
-	DailyDiaryOutside   int
-	DailyDiaryActive    int
-	DailyDiaryCreative  int
-	DailyDiaryListen    int
-	DailyDiaryGoneBetter int
 	DailyDiaryLog       int
 	TodoLinesInTodoPage int
 	IndexMeetingNotes   int
